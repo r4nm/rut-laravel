02ce3f1827ab0d464e1ec7290830d18dd2a58f0c
rnann <rnannigm@gmail.com>
2018-05-08 14:18:35 -0300

    Create Rut.class.php

---
+++ rnm/src/Providers/Rut.class.php
@@ -0,0 +1,42 @@
+<?php
+/* Copyright (c) 2016 Rodrigo Nannig
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace Rnm\Providers;
+ 
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\ServiceProvider;
+
+class Rut extends ServiceProvider {
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot() {
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register() {
+        \App::bind('rut', function() {
+            return new \Rnm\Rut();
+        });
+    }
+}
+?>
